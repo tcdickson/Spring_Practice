bdaaac25de21818dd8a2544389ef05bda9929c35
tcdickson <tylerdickson17@gmail.com>
2024-09-27 13:43:14 -0600

    refactored

---
+++ 02_appy/src/test/java/com/springPractice/appy/AppyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springPractice.appy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ 04_appy_buldJar/src/main/java/com/springPractice/appy/AppyApplication.java
@@ -0,0 +1,13 @@
+package com.springPractice.appy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppyApplication.class, args);
+	}
+
+}
